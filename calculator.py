1c32b257de10a3e2a8ef073c4f2ece582e54d9b9
TomBrnr <thomabrunner@tsn.at>
2026-03-27 10:41:55 +0100

    Add substraction Function

--- calculator.py
+++ calculator.py
@@ -1,5 +1,10 @@
 def add(a, b):
     return a + b
+def subtract(a, b):
+    return a - b
 
+
+# Erweitere auch den Print-Befehl unten:
 if __name__ == "__main__":
     print("5 + 3 =", add(5, 3))
+    print("10 - 4 =", subtract(10, 4))
